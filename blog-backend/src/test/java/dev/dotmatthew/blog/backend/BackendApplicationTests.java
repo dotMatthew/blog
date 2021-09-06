81a3ba453dec2fe5e802e824a636143c975fb0dc
Mathias Dollenbacher <hello@mdollenbacher.net>
2021-09-06 19:37:20 +0200

    Initial commit

---
+++ blog-backend/src/test/java/dev/dotmatthew/blog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dotmatthew.blog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
